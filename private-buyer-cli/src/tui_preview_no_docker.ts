c688c24da7d76c3dcafcc8f7bd9c815f8ffa46e6
erick <erickrs2esuarez@gmail.com>
2026-02-12 14:53:53 -0300

    dockers ok

--- private-buyer-cli/src/tui_preview_no_docker.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { createLogger } from './logger.js';
-import { run } from './cli.js';
-import { PreviewConfig } from './config.js';
-
-const config = new PreviewConfig();
-const logger = await createLogger(config.logDir);
-await run(config, logger);
--- private-buyer-cli/src/tui_standalone_no_docker.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { createLogger } from './logger.js';
-import { run } from './cli.js';
-import { UndeployedConfig } from './config.js';
-
-const config = new UndeployedConfig();
-const logger = await createLogger(config.logDir);
-await run(config, logger);
