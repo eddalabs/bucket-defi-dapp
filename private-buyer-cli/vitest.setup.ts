60b85196595cba932e0c4fc1badb53d4ed495785
erick <erickrs2esuarez@gmail.com>
2026-04-06 08:41:31 -0300

    starting with fresh contract

--- private-buyer-cli/vitest.setup.ts
+++
@@ -1,5 +0,0 @@
-import protobuf from 'protobufjs';
-import Long from 'long';
-
-protobuf.util.Long = Long;
-protobuf.configure();
--- private-buyer-contract/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * as PrivateBuyer from "./managed/private-buyer/contract/index.js";
-export * from "./witnesses";
